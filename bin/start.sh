e2933cfff1c86d84ddd4bb200e32c3a3c282e948
yuanzy <yuanzhiyi98@gmail.com>
2025-04-20 17:56:49 +0800

    ci:优化 GitHub Actions 中的 Java 项目构建流程

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,6 @@ JAVA_OPTS="-Djava.util.concurrent.ForkJoinPool.common.parallelism=20"
 JAR_NAME="fast-agent-admin-ranking-actions.jar"
 
 # start project
+ls
+chmod 777 $JAR_NAME
 java $JAVA_OPTS -jar $JAR_NAME
